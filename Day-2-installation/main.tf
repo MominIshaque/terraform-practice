9741c7bb069f727ef7b66db501994d398d2e5782
momin <cloudissak@gmail.com>
2025-07-07 19:06:49 +0530

    update

--- Day-2-installation/main.tf
+++ Day-2-installation/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
-    ami = "ami-05ffe3c48a9991133"
+    ami = "ami-0c803b171269e2d72"
     instance_type = "t2.micro"
-    key_name = "test"
+    key_name = "ohio-key"
   
 }
