036439a830c418c397d06680d0485d92d8689b90
Momin Ishaque <168473342+MominIshaque@users.noreply.github.com>
2025-07-17 17:57:33 +0000

    root child

---
+++ Day-10-Root-child-module/modules/ec2/variables.tf
@@ -0,0 +1,21 @@
+variable "ami_id" {
+    description = "ami id for ec2"
+    default = ""
+  
+}
+
+variable "instance_type" {
+    description = "instance type"
+    default = ""
+  
+}
+variable "tags" {
+    description = "name of instance"
+    default = ""
+  
+}
+variable "subnet_id" {
+    description = "subnet id"
+    default = ""
+  
+}
---
+++ Day-10-Root-child-module/variables.tf
@@ -0,0 +1,40 @@
+variable "cidr_block" {
+  default = ""
+
+}
+variable "subnet_cidr" {
+  default = ""
+
+}
+variable "az" {
+  default = ""
+
+}
+variable "ami_id" {
+  default = ""
+
+}
+variable "instance_type" {
+  default = ""
+
+}
+variable "subnet_id" {
+  default = ""
+
+}
+variable "instance_class" {
+  default = ""
+
+}
+variable "db_name" {
+  default = ""
+
+}
+variable "db_user" {
+  default = ""
+
+}
+variable "db_password" {
+  default = ""
+
+}
