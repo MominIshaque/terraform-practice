4d33c7717653c1f4ff241c700785f918eab848d1
Momin Ishaque <momin@example.com>
2025-07-21 15:15:17 +0530

    userdata

--- Day-11-userdata/shelscript.sh
+++ Day-11-userdata/shelscript.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -e
+
 yum update -y
 yum install -y httpd
+
 systemctl start httpd
 systemctl enable httpd
-echo "<h1>Deployed using Terraform + Shell Script</h1>" > /var/www/html/index.html
+
+cat <<EOF > /var/www/html/index.html
+<h1>Deployed using Terraform + Shell Script</h1>
+EOF
+systemctl restart httpd
