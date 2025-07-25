fdcb9a1e10f54bdf0dc7ca45a17e639804d85d0e
Momin Ishaque <momin@example.com>
2025-07-25 11:14:48 +0530

    jenkins update

--- Day-2-installation/main.tf
+++ Day-2-installation/main.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "name" {
-    ami = "ami-0c803b171269e2d72"
+    ami = "ami-08a6efd148b1f7504"
     instance_type = "t2.micro"
-    key_name = "ohio-key"
+    #key_name = "ohio-key"
+    tags = {
+        Name = "THROUGH TERRAFORM USING JENKINS"
+    }
   
 }
